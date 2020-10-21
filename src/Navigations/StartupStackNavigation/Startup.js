4e37583d82080db4a1253c350d944577b46dd169
ranjithraji <ranjthkumarranjith1999@gmail.com>
2020-10-21 10:38:39 +0530

    Added  tamplate for Yellofie front End

---
+++ src/Navigations/StartupStackNavigation/Startup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Startup = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Startup
+
+const styles = StyleSheet.create({})
