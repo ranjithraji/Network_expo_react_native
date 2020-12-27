c5758600da8ac26967bc361f94ca12e7f7b0a8b0
ranjith <ranjithkumarranjith1999@gmail.com>
2020-12-27 13:49:38 +0530

    Added home screen

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={{ flex: 1, marginVertical: 10 ,padding:10}}>
+                <View style={{ flex: 1, backgroundColor: 'white' }}>
+                    <View style={{flex:1, backgroundColor: 'white'}}>
+
+                    </View>
+                </View>
+                <View style={{ flex: 1, backgroundColor: 'red',marginTop:10, }}>
+
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
